a2b7dcef2df28240b3228ae0048cc5947f33ca2c
CarlosMendezzz <carlos.mendez@25-2.com>
2023-09-04 20:19:02 -0600

    Initial Commit

---
+++ PlantDesignApp/PlantDesignAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantDesignAppApp.swift
+//  PlantDesignApp
+//
+//  Created by Carlos Mendez on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct PlantDesignAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
