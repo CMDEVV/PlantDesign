0189326e778622ae030ca21b917eb3f331bcf5fb
CarlosMendezzz <carlos.mendez@25-2.com>
2023-09-04 22:14:49 -0600

    initalcommit

---
+++ PlantDesignApp/Model/Plant.swift
@@ -0,0 +1,8 @@
+//
+//  Plant.swift
+//  PlantDesignApp
+//
+//  Created by Carlos Mendez on 9/4/23.
+//
+
+import Foundation
---
+++ PlantDesignApp/View/CustomCarousel.swift
@@ -0,0 +1,20 @@
+//
+//  CustomCarousel.swift
+//  PlantDesignApp
+//
+//  Created by Carlos Mendez on 9/4/23.
+//
+
+import SwiftUI
+
+struct CustomCarousel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomCarousel_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomCarousel()
+    }
+}
